084130b0140898224cf85df7c7d0e44be344d3a6
Cameron Drummond <48034712+CamCommand@users.noreply.github.com>
2021-04-27 18:34:55 -0400

    Update Gen.py

--- Gen.py
+++ Gen.py
@@ -1,15 +1,36 @@
 import random
+import sys
+import psutil
+import os
 
-#put your file location paths here, C:\\Users\\username\\Desktop\\CodeFolder\\CharacterPrompt\\Names.txt
-txt1=open("location of file of Names","r")
-txt2=open("location of file of Emotions","r")
-txt3=open("location of file of Nouns","r")
-m1=txt1.readlines()# read the lines of the txt files
+path=input("Enter the path that the code is located in, example- C:\\Users\\Name\\Desktop\\CharacterGen : ")
+path1=path + "\\Names.txt"
+path2=path + "\\Emotions.txt"
+path3=path + "\\Nouns.txt"
+print("\n")
+
+num=input("Enter how many character prompts you desire: ")
+print("\n")
+
+while int(num) <= 0:# loop until a valid number is entered
+    num=input("Enter how many character prompts you desire above zero: ")
+    print("Invaild value entered")
+
+try:# try catch incase the filepath is wrong
+    txt1=open(path1,"r")
+    txt2=open(path2,"r")
+    txt3=open(path3,"r")
+except FileNotFoundError:
+    print("File does not exist or folder not found.")
+    exit()
+    
+m1=txt1.readlines()
 m2=txt2.readlines()
 m3=txt3.readlines()
-l1=[]# create lists
+l1=[]
 l2=[]
 l3=[]
+
 for i in range(0,len(m1)-1):# I use 3 different for loops to easily see which list is beinging looped through and also the lists are different sizes
     x=m1[i]
     z=len(x)
@@ -28,11 +49,11 @@ for i in range(0,len(m3)-1):
     a=x[:z-1]
     l3.append(a)
 l3.append(m3[i+1])
-for j in range(0, 9):# print ten prompts
+for j in range(0, int(num)):# output the desired number of prompts
     o1=random.choice(l1)# random choices from the list of nouns
     o2=random.choice(l2)
     o3=random.choice(l3)
-    print(o1, "is feeling",o2, "because of her", o3)# print the sentences
+    print(o1, "is feeling",o2, "because of her", o3)
 txt1.close()# close files
 txt2.close()
 txt3.close()
